7d93a7af56b5fb4dad2987867bcf800d9d2b1f0a
KalyanArun <kalyanarunachalam@gmail.com>
2023-01-25 11:53:49 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import Home from "./Home";
 import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <div className='content'>
-        <Home />
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route exact path='/' component={Home}>
+              <Home />
+            </Route>
+            <Route path='/create' component={Create}>
+              <Create />
+            </Route>
+            <Route path='/blogs/:id' component={BlogDetails}>
+              <BlogDetails />
+            </Route>
+            <Route path='*'>
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link, link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <h1>Micro-Blog</h1>
       <div className='links'>
-        <a href='/'>Home</a>
-        <a href='/create'>Create</a>
+        <Link to='/'>Home</Link>
+        <Link to='/create'>Create</Link>
       </div>
     </nav>
   );
